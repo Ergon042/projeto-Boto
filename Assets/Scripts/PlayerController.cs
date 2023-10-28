d4c3394c97b481fe436bd778a284ca764b0438f0
ArturToledo <97695066+ArturToledo@users.noreply.github.com>
2023-10-28 15:13:14 -0300

    Shooting
    Atire nos inimigos pra dar dano

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,7 +13,7 @@ public class PlayerController : MonoBehaviour
 
     public float mouseSensitivity = 1f;
 
-    public Transform viewCam;
+    public Camera viewCam;
 
     // Start is called before the first frame update
     void Start()
@@ -31,12 +31,26 @@ public class PlayerController : MonoBehaviour
         Vector3 moveVertical = transform.right * moveInput.y;  
 
         theRB.velocity = (moveHorizontal + moveVertical) * moveSpeed;
+
          //player view control
-         mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
 
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z - mouseInput.x);
 
-        viewCam.localRotation = Quaternion.Euler(viewCam.localRotation.eulerAngles + new Vector3(0f, mouseInput.y,0f));
+        viewCam.transform.localRotation = Quaternion.Euler(viewCam.transform.localRotation.eulerAngles + new Vector3(0f, mouseInput.y,0f));
 
+        //Shooting
+            if(Input.GetMouseButton(0)){
+                Ray ray = viewCam.ViewportPointToRay(new Vector3(.5f, .5f, 0f));
+                RaycastHit hit;
+                if(Physics.Raycast(ray, out hit))
+                {
+                    Debug.Log(" I'm looking at " + hit.transform.name);
+                } else
+                {
+                    Debug.Log(" I'm looking at nothing!");
+                }
+            }
     }
 }
